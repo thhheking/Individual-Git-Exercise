4977df377e53ed656d9a119b4b8bf4da8edf0f07
thhheking <tushar.kang@gmail.com>
2024-04-08 21:56:51 -0700

    Comment
    adding a comment on file

---
+++ StringArraySortingExample.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class StringArraySortingExample {
+    public static void main(String[] args) {
+        String[] array = {"banana", "apple", "orange", "grape", "kiwi"};
+        
+        // Sorting the array I added comment 
+        Arrays.sort(array);
+        
+        // Printing the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
